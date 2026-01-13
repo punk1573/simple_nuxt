3af225f6eb8652bf3374277f6e3129281d6fb5f9
haoqiang.peng <punk1573@hotmail.com>
2026-01-14 00:22:19 +0800

    update for esa pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true }
+    nitro: {
+		// 明确告知 Nitro，这些模块是 Node.js 环境提供的，不要打包
+		externals: {
+			external: builtinModules
+		}
+	}
 })
